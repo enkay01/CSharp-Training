d05a1d50364249aafbc391bb6f3ea965d63bc3bd
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-08 14:27:53 +0000

    Completed exercise 5.1 and 5.2

--- First Program/ConsoleApp1/ConsoleApp1/Program.cs
+++ First Program/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,11 +10,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            times_tables();
+            Console.ReadLine();
+        }
+        public static void times_tables()
+        {
+            Console.WriteLine("Hello World! Please enter a number");
+            int max = int.Parse(Console.ReadLine());
+            int res = 0;
+            for(int i = 2; i <= max; i++)
+            {
+                for(int j = 2; j <= max; j++)
+                {
+                    res = i * j;
+                    Console.WriteLine("{0} X {1} = {2}", j, i, res);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        public static void sum_of_all()
+        {
+            int max = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int c = 1; c <= max; c++)
+            {
+                sum += c;
+            }
+            Console.WriteLine("The sum of numbers 1 to {0} is: {1}", max, sum);
+        }
+        public static void nameFunction()
+        {
             Console.WriteLine("Hello World! Please enter your first name");
             string name1 = Console.ReadLine();
             Console.WriteLine("Please enter your second name");
             string name2 = Console.ReadLine();
-            if(name1.Equals("") && name2.Equals(""))
+            if (name1.Equals("") && name2.Equals(""))
             {
                 Console.WriteLine("You didn't enter a name");
             }
@@ -27,7 +57,7 @@ namespace ConsoleApp1
                 Console.WriteLine("Hello, {0} {1}!", name1, name2);
             }
             Console.ReadLine();
+
         }
     }
-    
 }
