3b1aa78dc7369abb043bb00ce1dc16aa09ed3857
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-08 13:01:35 +0000

    Added functionality to choose operation in calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,24 +8,48 @@ namespace Calculator
         {
             Calculator c1 = new Calculator();
             Console.WriteLine("Hello World!");
-            int res = c1.getProduct();
-            Console.WriteLine(res);
+            c1.getOperator();
+            c1.getInputs();
+            int res = c1.doOp();
+            Console.WriteLine("The result is {0}", res);
             Console.ReadLine();
         }
     }
     class Calculator
     {
+        char op = '+';
+        int n1;
+        int n2;
         public Calculator()
         {
 
         }
-        public int getProduct()
+        public void getOperator()
+        {
+            Console.WriteLine("Please enter the operator");
+            char tOp = Console.ReadLine().ToCharArray()[0];
+            if (tOp == '+' || tOp == '-' || tOp == '*' || tOp == '/')
+                op = tOp;
+            
+        }
+        public void getInputs()
         {
             Console.WriteLine("Enter number 1");
-            int n1 = int.Parse(Console.ReadLine());
+            this.n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter number 2");
-            int n2 = int.Parse(Console.ReadLine());
-            return n1 * n2;
+            this.n2 = int.Parse(Console.ReadLine());
+            
+        }
+        public int doOp()
+        {
+            if (this.op == '+')
+                return this.n1 + this.n2;
+            else if (this.op == '-')
+                return this.n1 - this.n2;
+            else if (this.op == '*')
+                return this.n1 * this.n2;
+            else
+                return this.n1 / this.n2;
         }
     }
 }
