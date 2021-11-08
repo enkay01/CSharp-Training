de637a268bd858e882b082e305e115e5e6f08536
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-08 11:51:32 +0000

    Created Calculator project

--- First Program/ConsoleApp1/ConsoleApp1/Program.cs
+++ First Program/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string aFriend = Console.ReadLine();
+            Console.WriteLine("Hello World! Please enter one number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Please enter another number.");
+            string number2 = Console.ReadLine();
+            int n1 = int.Parse(number1);
+            int n2 = int.Parse(number2);
+            int res = n1 * n2;
+            Console.WriteLine("The result is: {0}", res);
+            Console.ReadLine();
+        }
+    }
+    class Calculator
+    {
+        public Calculator()
+        {
         }
     }
 }
