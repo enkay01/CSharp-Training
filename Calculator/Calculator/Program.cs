a3ec43e81fe62415471befcd0dc5f7b01fd06de7
IAS\Ian.Nkwocha <ian.nkwocha@iris.co.uk>
2021-11-08 14:44:30 +0000

    Completed exercise 5.3

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,26 +7,28 @@ namespace Calculator
         static void Main(string[] args)
         {
             Calculator c1 = new Calculator();
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the calculator\n=============================");
             c1.getOperator();
             c1.getInputs();
             int res = c1.doOp();
-            Console.WriteLine("The result is {0}", res);
+            Console.WriteLine("The answer is {0}", res);
             Console.ReadLine();
+
         }
     }
     class Calculator
     {
         char op = '+';
+        int noNums;
         int n1;
-        int n2;
+        string operands = "";
         public Calculator()
         {
 
         }
         public void getOperator()
         {
-            Console.WriteLine("Please enter the operator");
+            Console.WriteLine("Please enter the operator.");
             char tOp = Console.ReadLine().ToCharArray()[0];
             if (tOp == '+' || tOp == '-' || tOp == '*' || tOp == '/')
                 op = tOp;
@@ -34,22 +36,39 @@ namespace Calculator
         }
         public void getInputs()
         {
-            Console.WriteLine("Enter number 1");
-            this.n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter number 2");
-            this.n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many numbers do you want to {0}?", op);
+            this.noNums = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= noNums; i++)
+            {
+                Console.WriteLine("Please enter number {0}:", i);
+                operands = operands + Console.ReadLine().ToCharArray()[0];
+                if (i == 1)
+                    n1 = i;
+                    
+            }
             
         }
         public int doOp()
         {
-            if (this.op == '+')
-                return this.n1 + this.n2;
-            else if (this.op == '-')
-                return this.n1 - this.n2;
-            else if (this.op == '*')
-                return this.n1 * this.n2;
-            else
-                return this.n1 / this.n2;
+            int runningT = n1;
+            char[] numList = operands.ToCharArray();
+            for(int c = 1; c <operands.Length; c++)
+            {
+                String s = numList[c].ToString();
+                int x = int.Parse(s);
+
+                if (this.op == '+')
+                    runningT += x;
+                else if (this.op == '-')
+                    runningT -= x;
+                else if (this.op == '*')
+                    runningT *= x;
+                else
+                    runningT /= x;
+            }
+            return runningT;
+            
         }
+
     }
 }
